9a262fce25b5bdcc63a9ee81d0c55e812d72537a
nikola <nikola.popovicc99@gmail.com>
2024-11-13 13:12:59 +0100

    canvas dynamic aspect ratio

---
+++ src/main.ts
@@ -0,0 +1,43 @@
+import "./style.css";
+
+const root = document.getElementById("root") as HTMLDivElement;
+root.innerHTML = `
+  <canvas id="app"/>
+`;
+
+let width = -1;
+let height = -1;
+
+const canvas = document.getElementById("app") as HTMLCanvasElement;
+
+const gameAspectRatio = 16 / 10;
+
+function handleCanvasAspectRatio() {
+  if (width === root.clientWidth && height === root.clientHeight) {
+    return;
+  }
+
+  width = root.clientWidth;
+  height = root.clientHeight;
+
+  const windowAspectRatio = width / height;
+
+  if (windowAspectRatio >= gameAspectRatio) {
+    Object.assign(canvas.style, {
+      width: `${height * gameAspectRatio}px`,
+      height: `${height}px`,
+    } as CSSStyleDeclaration);
+  } else {
+    Object.assign(canvas.style, {
+      width: `${width}px`,
+      height: `${width / gameAspectRatio}px`,
+    } as CSSStyleDeclaration);
+  }
+}
+
+function gameLoop() {
+  handleCanvasAspectRatio();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
